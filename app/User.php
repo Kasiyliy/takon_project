2caf1a95fa02ae50ff23b49c44de9ecd4ff960bd
air <asilkhan-al@mail.ru>
2019-11-25 16:10:52 +0600

    User functions added

--- app/User.php
+++ app/User.php
@@ -37,28 +37,75 @@ class User extends Authenticatable
         'username_verified_at' => 'datetime',
     ];
 
-    public function isAdmin(){
+    //FUNCTIONS
+
+    public function isAdmin()
+    {
         return $this->role_id == Role::ROLE_ADMINISTRATOR_ID;
     }
 
-    public function isPartner(){
+    public function isPartner()
+    {
         return $this->role_id == Role::ROLE_PARTNER_ID;
     }
 
-    public function isCompanyJM(){
+    public function isCompanyJM()
+    {
         return $this->role_id == Role::ROLE_COMPANY_OR_JUDICIAL_MEMBER_ID;
     }
 
-    public function isMobileUser(){
+    public function isMobileUser()
+    {
         return $this->role_id == Role::ROLE_MOBILE_USER_ID;
     }
 
-    public function isCashier(){
+    public function isCashier()
+    {
         return $this->role_id == Role::ROLE_CASHIER_ID;
     }
+
+    //RELATIONSHIPS
 
     public function role()
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
+
+    public function cashier()
+    {
+        return $this->hasOne(Cashier::class, 'user_id', 'id');
+    }
+
+    public function company()
+    {
+        return $this->hasOne(Company::class, 'user_id', 'id');
+    }
+
+    public function partner()
+    {
+        return $this->hasOne(Partner::class, 'user_id', 'id');
+    }
+
+    public function mobileUser()
+    {
+        return $this->hasOne(MobileUser::class, 'user_id', 'id');
+    }
+
+    public function getRealUserTypeByRole()
+    {
+        if ($this->isAdmin()) {
+            return $this;
+        } else if ($this->isCashier()) {
+            return $this->cashier();
+        } else if ($this->isCompanyJM()) {
+            return $this->company();
+        } else if ($this->isMobileUser()) {
+            return $this->mobileUser();
+        } else if ($this->isPartner()) {
+            return $this->partner();
+        } else {
+            throw new \Exception("ROLE MISCONFIGURATION", 400);
+        }
+    }
+
 }
