c645432ce1d8e96421f7449e1d288ee30137c409
air <asilkhan-al@mail.ru>
2019-12-15 23:19:50 +0600

    API VERSIONING ADDED

--- routes/api/api_cashier.php
+++ routes/api/api_cashier.php
@@ -6,7 +6,23 @@
  * Time: 12:37
  */
 
+//К ТВОЕЙ АПИШКЕ ДОБАВЛЯЕТСЯ ВЕРСИОННОСТЬ у всех ендпоинтов будет префикс v1....vN
+//СОЗДАВАЙ ВСЕ ВНУТРИ ПАПОК с версионностью v1.....vN
 
-Route::group(['middleware' => ['token', 'ROLE_CASHIER']], function () {
+Route::group(['namespace' => 'v1', 'prefix' => 'v1'], function () {
 
+    Route::post('/auth/login', ['as' => 'auth.login', 'uses' => 'ApiController@logIn'])->where('id', '[0-9]+');
+
+    Route::group(['middleware' => ['token', 'ROLE_CASHIER']], function () {
+
+    });
+});
+
+//ШАБЛОН ДЛЯ ДАЛЬНЕЙШИХ АПИШЕК
+Route::group(['namespace' => 'v2', 'prefix' => 'v2'], function () {
+
+
+    Route::group(['middleware' => ['token', 'ROLE_CASHIER']], function () {
+
+    });
 });
--- routes/api/api_mobile_user.php
+++ routes/api/api_mobile_user.php
@@ -5,9 +5,23 @@
  * Date: 25.11.2019
  * Time: 12:35
  */
-Route::post('/auth/login', ['as' => 'auth.login', 'uses' => 'ApiController@logIn'])->where('id', '[0-9]+');
+
+//К ТВОЕЙ АПИШКЕ ДОБАВЛЯЕТСЯ ВЕРСИОННОСТЬ у всех ендпоинтов будет префикс v1....vN
+////СОЗДАВАЙ ВСЕ ВНУТРИ ПАПОК с версионностью v1.....vN
+Route::group(['namespace' => 'v1', 'prefix' => 'v1'], function () {
+
+    Route::post('/auth/login', ['as' => 'auth.login', 'uses' => 'ApiController@logIn'])->where('id', '[0-9]+');
+
+    Route::group(['middleware' => ['token', 'ROLE_MOBILE_USER']], function () {
+
+    });
+});
+
+//ШАБЛОН ДЛЯ ДАЛЬНЕЙШИХ АПИШЕК
+Route::group(['namespace' => 'v2', 'prefix' => 'v2'], function () {
 
 
-Route::group(['middleware' => ['token', 'ROLE_MOBILE_USER']], function () {
+    Route::group(['middleware' => ['token', 'ROLE_MOBILE_USER']], function () {
 
+    });
 });
