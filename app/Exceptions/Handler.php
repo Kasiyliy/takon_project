f7dae6a557e35545410c7e5d3891b4378890b145
Tair <adibek.t@maint.kz>
2019-12-26 19:31:17 +0600

    Handler changed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,129 +9,85 @@ use Illuminate\Support\Facades\Session;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+	/**
+	 * A list of the exception types that are not reported.
+	 *
+	 * @var array
+	 */
+	protected $dontReport = [
+		//
+	];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * A list of the inputs that are never flashed for validation exceptions.
+	 *
+	 * @var array
+	 */
+	protected $dontFlash = [
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
+	/**
+	 * Report or log an exception.
+	 *
+	 * @param \Exception $exception
+	 * @return void
+	 */
+	public function report(Exception $exception)
+	{
+		parent::report($exception);
+	}
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \Exception $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        if ($request->wantsJson() || \Illuminate\Support\Facades\Request::is('api/*')) {
-            return $this->handleApiException($request, $exception);
-        } else {
-            return $this->handleWebExeception($request, $exception);
-        }
+	/**
+	 * Render an exception into an HTTP response.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Exception $exception
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $exception)
+	{
+		if ($request->wantsJson() || \Illuminate\Support\Facades\Request::is('api/*')) {
+			return $this->handleApiException($request, $exception);
+		} else {
+			return $this->handleWebExeception($request, $exception);
+		}
 
 
-    }
+	}
 
-    private function handleWebExeception($request, Exception $exception)
-    {
+	private function handleWebExeception($request, Exception $exception)
+	{
 
-        if ($exception instanceof WebServiceException) {
-            return redirect()->back()->withErrors($exception->getValidator())->withInput();
-        }
+		if ($exception instanceof WebServiceException) {
+			return redirect()->back()->withErrors($exception->getValidator())->withInput();
+		}
 
-        if ($exception instanceof WebServiceErroredException) {
-            return redirect()->back()->with('error', $exception->getExplanation());
-        }
-        return parent::render($request, $exception);
-    }
+		if ($exception instanceof WebServiceErroredException) {
+			return redirect()->back()->with('error', $exception->getExplanation());
+		}
+		return parent::render($request, $exception);
+	}
 
-    private function handleApiException($request, Exception $exception)
-    {
-        $exception = $this->prepareException($exception);
+	private function handleApiException($request, Exception $exception)
+	{
+		$exception = $this->prepareException($exception);
 
-        if ($exception instanceof \App\Exceptions\ApiServiceException) {
-            return $exception->getApiResponse();
-        }
+		if ($exception instanceof \App\Exceptions\ApiServiceException) {
+			return $exception->getApiResponse();
+		}
 
-        if ($exception instanceof Illuminate\Http\Exceptions\HttpResponseException) {
-            $exception = $exception->getResponse();
-        }
 
-        if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
-            $exception = $this->unauthenticated($request, $exception);
-        }
+		if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
+			$exception = $this->unauthenticated($request, $exception);
+		}
 
-        if ($exception instanceof \Illuminate\Validation\ValidationException) {
-            $exception = $this->convertValidationExceptionToResponse($exception, $request);
-        }
+		if ($exception instanceof \Illuminate\Validation\ValidationException) {
+			$exception = $this->convertValidationExceptionToResponse($exception, $request);
+		}
 
-        return $this->customApiResponse($exception);
-    }
+		return parent::render($request, $exception);
+	}
 
-    private function customApiResponse(Exception $exception)
-    {
-        if (method_exists($exception, 'getStatusCode')) {
-            $statusCode = $exception->getStatusCode();
-        } else {
-            $statusCode = 500;
-        }
-
-        $response = [];
-
-        switch ($statusCode) {
-            case 401:
-                $response['message'] = 'Unauthorized';
-                break;
-            case 403:
-                $response['message'] = 'Forbidden';
-                break;
-            case 404:
-                $response['message'] = 'Not Found';
-                break;
-            case 405:
-                $response['message'] = 'Method Not Allowed';
-                break;
-            case 422:
-                $response['message'] = $exception->original['message'];
-                $response['errors'] = $exception->original['errors'];
-                break;
-            default:
-                $response['message'] = ($statusCode == 500) ? 'Whoops, looks like something went wrong' : $exception->getMessage();
-                break;
-        }
-
-        if (config('app.debug')) {
-            $response['trace'] = $exception->getTrace();
-            $response['code'] = $exception->getCode();
-        }
-
-        $response['status'] = $statusCode;
-
-        return response()->json($response, $statusCode);
-    }
 }
